6f95a88e6eb07597ac2471fa453fd23228b7680a
SuperNova911 <suwhan77@naver.com>
2019-05-19 01:55:33 +0900

    컨트롤 관련

---
+++ Tetris/Control.h
@@ -0,0 +1,9 @@
+#pragma once
+
+typedef enum UserInput_e
+{
+	Input_MoveUp, Input_MoveRight, Input_MoveDown, Input_MoveLeft, 
+	Input_RotateRight, Input_RotateLeft, 
+	Input_DropDown,
+	Input_Pause, Input_Special
+} UserInput;
