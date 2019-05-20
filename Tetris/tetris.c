6b9fd9ce206c432253d418555d16db38ada3f0ea
SuperNova911 <suwhan77@naver.com>
2019-05-21 00:26:53 +0900

    Map, BlockBag을 따로 작성

--- Tetris/tetris.c
+++ Tetris/tetris.c
@@ -4,271 +4,45 @@
 #include <time.h>
 #include "Block.h"
 #include "Control.h"
+#include "Map.h"
+#include "Point.h"
 #include "Tetris.h"
-#include "Utility.h"
 
-const Point SPAWN_POSITION = { MAP_COL / 2 - BLOCK_SHAPE_COL / 2, 0 };
-
-BlockTile Map[MAP_ROW][MAP_COL];
-bool ShowGhostBlock;
-
-BlockTile BlockBag[BLOCK_BAG_NUMBER];
-int BlockBagCount;
-
-Block CurrentBlock;
-Block NextBlock;
-
-void InitializeTetris()
+void InitializeTetris(TetrisGame* tetris)
 {
-	srand((unsigned)time(NULL));
-	
-	ShowGhostBlock = false;
-
-	ClearMap();
-	ClearBlockBag();
+	InitializeMap(&tetris->GameMap);
+	InitializeBlockBag(&tetris->BlockBag);
 }
 
-void UpdateTetris()
-{
-	// 
-}
-
-bool IsOutOfMap(Block* block, Point targetPosition)
-{
-	int row, col;
-
-	// Quick Check
-	if (block->Position.y < 0 || block->Position.y + BLOCK_SHAPE_ROW > MAP_ROW ||
-		block->Position.x < 0 || block->Position.x + BLOCK_SHAPE_COL > MAP_COL)
-	{
-		return true;
-	}
-
-	for (row = 0; row < BLOCK_SHAPE_ROW; row++)
-	{
-		for (col = 0; col < BLOCK_SHAPE_COL; col++)
-		{
-			if (block->Shape[row][col] == 0)
-			{
-				continue;
-			}
-
-			if (block->Position.y + row < 0 || block->Position.y + row > MAP_ROW ||
-				block->Position.x + col < 0 || block->Position.x + col > MAP_COL)
-			{
-				return true;
-			}
-		}
-	}
-
-	return false;
-}
-
-bool IsCollide(Block* block, Point targetPosition)
-{
-	int row, col;
-
-	if (IsOutOfMap(block, targetPosition) == true)
-	{
-		return true;
-	}
-
-	for (row = 0; row < BLOCK_SHAPE_ROW; row++)
-	{
-		for (col = 0; col < BLOCK_SHAPE_COL; col++)
-		{
-			if (block->Shape[row][col] == 0)
-			{
-				continue;
-			}
-
-			// IsOutOfMap() 에서 Map 범위를 벗어나는지 검사하므로 따로 범위검사 안 함
-			if (Map[block->Position.y + row][block->Position.x + col] != BlockTile_Empty)
-			{
-				return true;
-			}
-		}
-	}
-
-	return false;
-}
-
-void ClearMap()
-{
-	memset(Map, 0, MAP_SIZE);
-}
-
-void RenderToBoolMap(bool renderedMap[][MAP_COL])
-{
-	int row, col;
-
-	memcpy(renderedMap, Map, MAP_SIZE);
-
-	if (CurrentBlock.IsValid == false)
-	{
-		return;
-	}
-
-	for (row = 0; row < BLOCK_SHAPE_ROW; row++)
-	{
-		for (col = 0; col < BLOCK_SHAPE_COL; col++)
-		{
-			renderedMap[row + CurrentBlock.Position.y][col + CurrentBlock.Position.x] = CurrentBlock.Shape[row][col];
-		}
-	}
-}
-
-void SetGhostBlockVisibility(bool showGhostBlock)
-{
-	ShowGhostBlock = showGhostBlock;
-}
-
-void GenerateBlockBag()
-{
-	BlockTile randomTile;
-	bool existChecker[BLOCK_TILE_NUMBER];
-
-	ClearBlockBag();
-	memset(existChecker, 0, sizeof(bool) * BLOCK_TILE_NUMBER);
-
-	while (true)
-	{
-		randomTile = rand() % BLOCK_TILE_NUMBER + 1;
-
-		if (existChecker[randomTile - 1] == true)
-		{
-			continue;
-		}
-
-		existChecker[randomTile - 1] = true;
-		BlockBag[BlockBagCount] = randomTile;
-		BlockBagCount++;
-
-		if (BlockBagCount == BLOCK_TILE_NUMBER)
-		{
-			break;
-		}
-	}
-}
-
-void ClearBlockBag()
-{
-	memset(BlockBag, 0, BLOCK_BAG_SIZE);
-	BlockBagCount = 0;
-}
-
-bool IsBlockBagEmpty()
-{
-	return BlockBagCount <= 0 ? true : false;
-}
-
-Block PrepareNextBlock()
-{
-	BlockTile nextBlockTile;
-	const Point DEFAULT_POSITION = { 0, 0 };
-
-	if (IsBlockBagEmpty() == true)
-	{
-		GenerateBlockBag();
-	}
-
-	BlockBagCount--;
-	nextBlockTile = BlockBag[BlockBagCount];
-	BlockBag[BlockBagCount] = BlockTile_Empty;
-
-	BuildBlock(&NextBlock, nextBlockTile, BlockDirection_Up, DEFAULT_POSITION);
-}
-
-void SpawnNextBlock()
-{
-	CurrentBlock = NextBlock;
-	CurrentBlock.Position = SPAWN_POSITION;
-
-	PrepareNextBlock();
-
-	if (IsCollide(&CurrentBlock, CurrentBlock.Position) == true)
-	{
-		// TODO: GameOver
-		printf("GameOver\n");
-		return;
-	}
-
-	CurrentBlock.IsValid = true;
-}
-
-void SpawnGarbage()
+void RunTetris(TetrisGame* tetris)
 {
 }
 
-void MoveBlock(MoveDirection direction)
+void UpdateTetris(TetrisGame* tetris)
 {
-	const Point PRESET[MOVE_DIRECTION_NUMBER] = { { 0, -1 }, { 1, 0 }, { 0, 1 }, { -1, 0 } };
-	Point movedPosition;
+	// Move, Rotate
 
-	if (CurrentBlock.IsValid == false)
-	{
-		return;
-	}
+	Gravity();
 
-	if (direction < Move_Up || direction > Move_Left)
-	{
-		return;
-	}
-
-	movedPosition = AddPoint(&CurrentBlock.Position, &PRESET[direction]);
-
-	if (IsCollide(&CurrentBlock, movedPosition) == true)
-	{
-		return;
-	}
-
-	CurrentBlock.Position = movedPosition;
+	ClearLine();
 }
 
-void RotateBlock(RotateDirection direction)
+void Gravity()
 {
 	if (CurrentBlock.IsValid == false)
 	{
 		return;
 	}
 
-	RotateBlockShape(&CurrentBlock, direction);
-}
-
-void DropDownBlock()
-{
-	if (CurrentBlock.IsValid == false)
+	if (IsReady(&Core.GravityTimer) == true)
 	{
-		return;
+		MoveBlock(Move_Down);
 	}
 
-	CurrentBlock.Position = GetDropDownPosition(&CurrentBlock);
+
 }
 
-Point GetDropDownPosition(Block* block)
-{
-	int x, y;
-	Point dropDownPosition, testPosition;
-
-	dropDownPosition = block->Position;
-
-	for (x = block->Position.x, y = block->Position.y; y <= MAP_ROW - BLOCK_SHAPE_ROW; y++)
-	{
-		testPosition.x = x;
-		testPosition.y = y;
-
-		if (IsCollide(block, testPosition) == true)
-		{
-			break;
-		}
-
-		dropDownPosition = testPosition;
-	}
-
-	return dropDownPosition;
-}
-
-void HandleUserInput(UserInput input)
+void HandleUserInput(TetrisGame* tetris, UserInput input)
 {
 	switch (input)
 	{
--- Tetris/tetris.h
+++ Tetris/tetris.h
@@ -1,45 +1,39 @@
 #pragma once
 
 #include <stdbool.h>
-#include "Block.h"
+#include "BlockBag.h"
 #include "Control.h"
-#include "Utility.h"
+#include "Map.h"
+#include "Point.h"
+#include "TickTimer.h"
 
-#define MAP_ROW 10
-#define MAP_COL 7
-#define MAP_SIZE (sizeof(BlockTile) * MAP_ROW * MAP_COL)
-#define BLOCK_BAG_NUMBER 4
-#define BLOCK_BAG_SIZE (sizeof(BlockTile) * BLOCK_BAG_NUMBER)
-#define LOCK_DELAY 1000
-
-typedef struct GameTimer_t
+typedef struct TetrisCore_t
 {
-	TickTimer Gravity;
-	TickTimer Lock;
+	unsigned int Stage;
+	unsigned int Score;
+	unsigned int Gravity;
+	unsigned int LockDelay;
 
-} GameTimer;
+	TickTimer GravityTimer;
+	TickTimer LockTimer;
+} TetrisCore;
 
-void InitializeTetris();
-void UpdateTetris();
+typedef struct TetrisInfo_t
+{
+	unsigned int Score;
+	bool IsRunning;
+} TetrisInfo;
 
-bool IsOutOfMap(Block* block, Point targetPosition);
-bool IsCollide(Block* block, Point targetPosition);
+typedef struct TetrisGame_t
+{
+	TetrisMap GameMap;
+	TetrisInfo GameInfo;
+	TetrisBlockBag BlockBag;
+} TetrisGame;
 
-void ClearMap();
-void RenderToBoolMap(bool renderedMap[][MAP_COL]);
-void SetGhostBlockVisibility(bool showGhostBlock);
+void InitializeTetris(TetrisGame* tetris);
+void RunTetris(TetrisGame* tetris);
 
-void GenerateBlockBag();
-void ClearBlockBag();
-bool IsBlockBagEmpty();
-Block PrepareNextBlock();
+void UpdateTetris(TetrisGame* tetris);
 
-void SpawnNextBlock();
-void SpawnGarbage();
-
-void MoveBlock(MoveDirection direction);
-void RotateBlock(RotateDirection direction);
-void DropDownBlock();
-Point GetDropDownPosition(Block* block);
-
-void HandleUserInput(UserInput input);
+void HandleUserInput(TetrisGame* tetris, UserInput input);
