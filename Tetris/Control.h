72f70852f1552ad252b260ecb64364475578ae3e
Suwhan Kim <suwhan77@naver.com>
2019-05-21 12:34:42 +0900

    새로운 enum, struct 추가

--- Tetris/Control.h
+++ Tetris/Control.h
@@ -1,9 +1,26 @@
 #pragma once
 
-typedef enum UserInput_e
+#define INPUT_SOURCE_NUMBER 9
+
+typedef enum InputCommand_e
 {
-	Input_MoveUp, Input_MoveRight, Input_MoveDown, Input_MoveLeft, 
-	Input_RotateRight, Input_RotateLeft, 
-	Input_DropDown,
-	Input_Pause, Input_Special
-} UserInput;
+	Input_Pause = 0, Input_DropDown = 1, Input_Special = 2,
+	Input_MoveLeft = 3, Input_MoveDown = 4, Input_MoveRight = 5,
+	Input_RotateLeft = 6, Input_MoveUp = 7, Input_RotateRight = 8
+} InputCommand;
+
+typedef enum InputType_e
+{
+	InputType_None, InputType_Click, InputType_Hold
+} InputType;
+
+typedef struct InputInfo_t
+{
+	InputCommand Command;
+	InputType Type;
+} InputInfo;
+
+typedef struct InputCollection_t
+{
+	InputInfo Input[INPUT_SOURCE_NUMBER];
+} InputCollection;
