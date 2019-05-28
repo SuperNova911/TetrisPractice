a187dfbbc17d0dc2dce8f55f75869b49c06328f7
SuperNova911 <suwhan77@naver.com>
2019-05-28 12:43:01 +0900

    pthread를 사용하여 사용자 입력을 처리

--- Linux/Game.c
+++ Linux/Game.c
@@ -1,13 +1,16 @@
+#include <pthread.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "Control.h"
 #include "DeviceManager.h"
 #include "Tetris.h"
 #include "TickTimer.h"
 #include "Point.h"
 
 void GetUserInput(InputCollection* inputCollection);
+void* GetUserInput_Thread(void* inputManager);
 
 unsigned char DotMatrix[10][7];
 
@@ -15,23 +18,35 @@ int main()
 {
 	TickTimer updateTimer;
 	TetrisGame tetris;
-	InputCollection userInput;
 	bool map[MAP_ROW][MAP_COL];
 	int row, col;
+    pthread_t inputThread;
+    int status;
+    InputManager inputManager;
+    InputInfo inputInfo;
 
 	OpenDevice();
 
 	InitializeTetris(&tetris);
 	InitializeTickTimer(&updateTimer, 100);
+    InitializeInputManager(&inputManager);
 
 	RunTetris(&tetris);
+
+    inputThread = pthread_create(&inputThread, NULL, GetUserInput_Thread, (void *)&inputManager);
+    if (inputThread < 0)
+    {
+        printf("Failed to create UserInput thread, id: '%d'\n", (int)inputThread);
+    }
+
+    pthread_join(inputThread, (void **)&status);
 
 	while (true)
 	{
 		WaitNextTick(&updateTimer);
 
-	    GetUserInput(&userInput);
-		ReadUserInput(&tetris, &userInput);
+        inputInfo = Dequeue(&inputManager.InputQueue);
+        InputTest(&tetris, &inputInfo);
 
 		UpdateTetris(&tetris);
 
@@ -72,4 +87,21 @@ void GetUserInput(InputCollection* inputCollection)
 			inputCollection->Input[index].Type = InputType_None;
 		}
 	}
+}
+
+void* GetUserInput_Thread(void* inputManager)
+{
+    InputManager* manager = (InputManager *)inputManager;
+    unsigned char switchStatus[INPUT_SOURCE_NUMBER];
+    TickTimer inputTimer;
+
+    InitializeTickTimer(&inputTimer, 10);
+    memset(switchStatus, 0, sizeof(switchStatus));
+
+    while (true)
+    {
+        WaitNextTick(&inputTimer);
+        GetSwitchStatus(switchStatus);
+        HandleInput(manager, switchStatus);
+    }
 }
