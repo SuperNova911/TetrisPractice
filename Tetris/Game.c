8dab8bfb971063e3e7b8da7acf3206d3059be46e
SuperNova911 <suwhan77@naver.com>
2019-06-05 01:00:48 +0900

    레벨업, 일시정지 애니메이션 구현, 최종 점수 보여주는 애니메이션 구현

--- Tetris/Game.c
+++ Tetris/Game.c
@@ -1,14 +1,18 @@
+﻿#include <math.h>
 #include <process.h>
+//#include <pthread.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+//#include <unistd.h>
 #include <Windows.h>
+#include "Control.h"
 #include "DeviceManager.h"
 #include "DotAnimator.h"
+#include "Point.h"
 #include "Tetris.h"
 #include "TickTimer.h"
-#include "Point.h"
 
 #define GAME_UPDATE_SPEED 100
 #define INPUT_UPDATE_SPEED 1
@@ -21,16 +25,19 @@ void UpdateGame();
 
 void UpdateInputAsync();
 //void* UpdateSwitchInput(void* inputManager);
+unsigned _stdcall UpdateSwitchInput(void* arg);
 
 void DrawMap(unsigned char map[][MAP_COL]);
 void DrawNextBlock();
+void DrawGameOver();
+void DrawFinalScore();
 void UpdateDevice();
-unsigned _stdcall UpdateSwitchInput(void* arg);
 
 // Tetris
 TetrisGame Tetris;
 TickTimer GameUpdateTimer;
 unsigned int CurrentLevel;
+unsigned int FinalScore;
 
 // Input
 InputManager Input;
@@ -38,7 +45,12 @@ TickTimer InputUpdateTimer;
 //pthread_t InputUpdateThread;
 
 // Device
-unsigned char DotMatrix[10][7];
+unsigned char DotMatrix[DOT_MATRIX_ROW][DOT_MATRIX_COL];
+
+// Animator
+DotAnimator Animator;
+bool AnimateLevel;
+bool AnimatePause;
 
 int main(int argc, char* argv[])
 {
@@ -51,8 +63,11 @@ int main(int argc, char* argv[])
 	RunTimer(&GameUpdateTimer);
 	RunTimer(&InputUpdateTimer);
 
-	//UpdateInputAsync();
+	UpdateInputAsync();
 	UpdateGame();
+
+	DrawGameOver();
+	DrawFinalScore();
 
 	CloseDevice();
 
@@ -64,11 +79,16 @@ void Initialize()
 	InitializeTetris(&Tetris);
 	InitializeTickTimer(&GameUpdateTimer, GAME_UPDATE_SPEED);
 	CurrentLevel = 0;
+	FinalScore = 0;
 
 	InitializeInputManager(&Input);
 	InitializeTickTimer(&InputUpdateTimer, INPUT_UPDATE_SPEED);
 	//InputUpdateThread = pthread_create(&InputUpdateThread, NULL, UpdateSwitchInput, NULL);
 	_beginthreadex(NULL, 0, UpdateSwitchInput, 0, 0, NULL);
+
+	AnimateLevel = false;
+	AnimatePause = false;
+	InitializeDotAnimator(&Animator);
 }
 
 void SetOptions(int argc, char* argv[])
@@ -93,13 +113,49 @@ void UpdateGame()
 
 		UpdateTetris(&Tetris);
 
+		if (CurrentLevel < Tetris.GameInfo.Level)
+		{
+			CurrentLevel = Tetris.GameInfo.Level;
+			AnimateLevel = true;
+		}
+		AnimatePause = !Tetris.GameInfo.IsRunning;
+
 		RenderTetrisMap(&Tetris, DotMatrix);
+
+		if (AnimateLevel == true)
+		{
+			PauseTetris(&Tetris);
+			PauseTimer(&GameUpdateTimer);
+			PauseTimer(&InputUpdateTimer);
+
+			DrawLevel(&Animator, DotMatrix, CurrentLevel);
+			DrawMap(DotMatrix);
+			Sleep(1500);
+
+			ResumeTimer(&GameUpdateTimer);
+			ResumeTimer(&InputUpdateTimer);
+			ResumeTetris(&Tetris);
+
+			AnimateLevel = false;
+			continue;
+		}
+
+		if (AnimatePause == true)
+		{
+			DrawPause(&Animator, DotMatrix);
+			DrawMap(DotMatrix);
+
+			continue;
+		}
+
 		DrawMap(DotMatrix);
 		DrawNextBlock();
-		UpdateDevice();
+		//UpdateDevice();
 
 		if (IsTetrisGameOver(&Tetris) == true)
 		{
+			CurrentLevel = Tetris.GameInfo.Level;
+			FinalScore = Tetris.GameInfo.Score;
 			break;
 		}
 	}
@@ -113,20 +169,25 @@ void UpdateInputAsync()
 
 //void* UpdateSwitchInput(void* unused)
 //{
-//	unsigned char switchStatus[PUSH_SWITCH_NUMBER];
-//	memset(switchStatus, 0, sizeof(switchStatus));
+//    unsigned char switchStatus[PUSH_SWITCH_NUMBER];
+//    memset(switchStatus, 0, sizeof(switchStatus));
 //
-//	while (true)
-//	{
-//		WaitNextTick(&InputUpdateTimer);
-//		GetSwitchStatus(switchStatus);
-//		HandleInput(&Input, switchStatus);
-//	}
+//    while (true)
+//    {
+//        if (IsTimerReady(&InputUpdateTimer) == false)
+//        {
+//            continue;
+//        }
+//
+//        GetSwitchStatus(switchStatus);
+//        HandleInput(&Input, switchStatus);
+//    }
 //}
 
 void DrawMap(unsigned char map[][MAP_COL])
 {
 	SetDotMatrix(map);
+	UpdateDevice();
 }
 
 void DrawNextBlock()
@@ -137,10 +198,108 @@ void DrawNextBlock()
 	SetLED(renderedNextBlock);
 }
 
+void DrawGameOver()
+{
+	int row, col;
+	unsigned char matrix[DOT_MATRIX_ROW][DOT_MATRIX_COL];
+	unsigned char fullCol[DOT_MATRIX_COL] = { 1, 1, 1 ,1 ,1, 1 ,1 };
+
+	Sleep(1000);
+}
+
+int MakeNumberMatrix(bool* matrix[DOT_SMALL_FONT_ROW], int number)
+{
+	int digitCount;
+	int colLength;
+	int row, col;
+	int index, pivot;
+
+	digitCount = (number == 0) ? 1 : (int)log10(number) + 1;
+	colLength = digitCount * (DOT_SMALL_FONT_COL + 1);
+
+	for (row = 0; row < DOT_SMALL_FONT_ROW; row++)
+	{
+		matrix[row] = (bool*)malloc(sizeof(bool) * colLength);
+		memset(matrix[row], 0, sizeof(bool) * colLength);
+	}
+
+	for (index = digitCount - 1, pivot = 0; index >= 0; index--, pivot += DOT_SMALL_FONT_COL + 1)
+	{
+		for (row = 0; row < DOT_SMALL_FONT_ROW; row++)
+		{
+			for (col = 0; col < DOT_SMALL_FONT_COL; col++)
+			{
+				matrix[row][pivot + col] = DOT_SMALL_NUMBER_FONT[number / (int)pow(10, index)][row][col];
+			}
+		}
+		number -= number / (int)pow(10, index) * (int)pow(10, index);
+	}
+
+	return colLength;
+}
+
+void DrawFinalScore()
+{
+	const static int ITERATION = 3;
+	int iterator;
+	int pivot;
+	int row, col;
+	int marginRow = 2;
+	bool *scoreNumberMatrix[DOT_SMALL_FONT_ROW];
+	int scoreNumberColLength;
+
+	memset(DotMatrix, 0, DOT_MATRIX_SIZE);
+	scoreNumberColLength = MakeNumberMatrix(scoreNumberMatrix, FinalScore);
+	
+	for (iterator = 0; iterator < ITERATION; iterator++)
+	{
+		for (pivot = -DOT_MATRIX_COL; pivot <= DOT_SCORE_FONT_COL; pivot++)
+		{
+			for (row = 0; row < DOT_SCORE_FONT_ROW; row++)
+			{
+				for (col = 0; col < DOT_MATRIX_COL; col++)
+				{
+					if (pivot + col < 0 || pivot + col >= DOT_SCORE_FONT_COL)
+					{
+						DotMatrix[row + marginRow][col] = 0;
+					}
+					else
+					{
+						DotMatrix[row + marginRow][col] = DOT_SCORE_FONT[row][col + pivot];
+					}
+				}
+			}
+			DrawMap(DotMatrix);
+			Sleep(100);
+		}
+
+		for (pivot = -DOT_MATRIX_COL; pivot <= scoreNumberColLength; pivot++)
+		{
+			for (row = 0; row < DOT_SMALL_FONT_ROW; row++)
+			{
+				for (col = 0; col < DOT_MATRIX_COL; col++)
+				{
+					if (pivot + col < 0 || pivot + col >= scoreNumberColLength)
+					{
+						DotMatrix[row + marginRow][col] = 0;
+					}
+					else
+					{
+						DotMatrix[row + marginRow][col] = scoreNumberMatrix[row][col + pivot];
+					}
+				}
+			}
+			DrawMap(DotMatrix);
+			Sleep(200);
+		}
+		Sleep(1000);
+	}
+}
+
 void UpdateDevice()
 {
-	static const char *T = "●";
-	static const char *F = "○";
+	static const char* T = "●";
+	static const char* F = "○";
 	static int UPDATE_COUNTER = 0;
 
 	unsigned char led = GetLED();
