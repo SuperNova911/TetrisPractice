aa304d8e01501b7aadc9fb7d137e914d18e7f1f8
Suwhan Kim <suwhan77@naver.com>
2019-06-05 10:52:25 +0900

    DotAnimator 구조체 제거

--- Tetris/DotAnimator.h
+++ Tetris/DotAnimator.h
@@ -2,7 +2,6 @@
 
 #include <stdbool.h>
 #include "DeviceManager.h"
-#include "TickTimer.h"
 
 #define DOT_BIG_FONT_ROW 10
 #define DOT_BIG_FONT_COL 7
@@ -10,20 +9,11 @@
 #define DOT_SMALL_FONT_COL 3
 #define DOT_SCORE_FONT_ROW 5
 #define DOT_SCORE_FONT_COL 24
-#define BLINK_DELAY 500
-
-typedef struct DotAnimator_t
-{
-	TickTimer BlinkTimer;
-} DotAnimator;
-
-void InitializeDotAnimator(DotAnimator* animator);
-void DrawLevel(DotAnimator* animator, unsigned char matrix[][DOT_MATRIX_COL], unsigned int level);
-void DrawPause(DotAnimator* animator, unsigned char matrix[][DOT_MATRIX_COL]);
-void DrawScore(DotAnimator* animator, unsigned char matrix[][DOT_MATRIX_COL], unsigned int score);
 
 extern const unsigned char DOT_BIG_NUMBER_FONT[10][10];
 extern const unsigned char DOT_FULL[10];
 extern const unsigned char DOT_BLANK[10];
 extern const bool DOT_SMALL_NUMBER_FONT[10][DOT_SMALL_FONT_ROW][DOT_SMALL_FONT_COL];
 extern const bool DOT_SCORE_FONT[DOT_SCORE_FONT_ROW][DOT_SCORE_FONT_COL];
+extern const bool DOT_SMALL_ALPHABET_L_FONT[DOT_SMALL_FONT_ROW][DOT_SMALL_FONT_COL];
+extern const bool DOT_PAUSE_FONT[DOT_MATRIX_ROW][DOT_MATRIX_COL];
